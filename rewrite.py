c69667e38159337c8c7a3e433d5632c137e5ca99
howl <24992550+howlagon@users.noreply.github.com>
2023-04-04 23:23:50 -0400

    add square buttons, "automatic padding" button

--- rewrite.py
+++ rewrite.py
@@ -1,4 +1,5 @@
 import pygame, warnings
+from typing import Union
 
 pygame.font.init()
 FONT = pygame.font.Font('clangen.ttf', 16)
@@ -24,7 +25,6 @@ class RectButton():
         self.size = (size[0], size[1])
         self.surface = pygame.Surface(self.size, pygame.SRCALPHA)
         self.surface = self.surface.convert_alpha()
-        self.surface.fill((255, 255, 0))
         self.hover = hover
         self.unavailable = unavailable
         if unavailable:
@@ -118,7 +118,7 @@ class RectButton():
         self.surface.blit(self._edge(self.size[0]-20, flip=True, shadow=self.shadow[3]), (10, self.size[1]-6))
         
         # text
-        text_rect = self.text.get_rect(center=(self.size[0] / 2 + 1, self.size[1] / 2 + 1))
+        text_rect = self.text.get_rect(center=(self.size[0] / 2 + 1, self.size[1] / 2 + 2))
         
         # yell at you if the text will be offset by 1px
         if text_rect.width % 2 != 0 and self.size[0] % 2 == 0:
@@ -126,3 +126,86 @@ class RectButton():
         elif text_rect.width % 2 == 0 and self.size[0] % 2 != 0:
             warnings.warn(f'Text has an even width! Consider using an even width instead of an odd one.', Warning, stacklevel=3)
         self.surface.blit(self.text, text_rect)
+
+class SquareButton(RectButton):
+    def _corner(self, shadow_corner1: bool, shadow_corner2: bool, rounded: bool = True):
+        surface = pygame.Surface((10, 8), pygame.SRCALPHA)
+        surface = surface.convert_alpha()
+        if rounded:
+            # outline
+            pygame.draw.rect(surface, self.palette[1], (4, 0, 6, 2))
+            pygame.draw.rect(surface, self.palette[1], (2, 2, 2, 2))
+            pygame.draw.rect(surface, self.palette[1], (0, 4, 2, 4))
+            # fill
+            pygame.draw.rect(surface, self.palette[3], (4, 4, 4, 4))
+            # inline
+            pygame.draw.rect(surface, self.palette[2], (4, 2, 6, 2))
+            pygame.draw.rect(surface, self.palette[2], (2, 4, 4, 2))
+            pygame.draw.rect(surface, self.palette[2], (2, 4, 2, 4))
+            # shadow
+            if shadow_corner1:
+                pygame.draw.rect(surface, self.palette[4], (6, 4, 4, 2))
+                pygame.draw.rect(surface, self.palette[4], (4, 6, 2, 2))
+            elif shadow_corner2:
+                pygame.draw.rect(surface, self.palette[4], (4, 6, 2, 2))
+                pygame.draw.rect(surface, self.palette[4], (6, 4, 2, 2))
+            return surface
+
+        # outline
+        pygame.draw.rect(surface, self.palette[1], (0, 0, 10, 2))
+        pygame.draw.rect(surface, self.palette[1], (0, 0, 2, 8))
+        # inline
+        pygame.draw.rect(surface, self.palette[2], (2, 2, 8, 2))
+        pygame.draw.rect(surface, self.palette[2], (2, 2, 2, 6))
+        # fill
+        pygame.draw.rect(surface, self.palette[3], (4, 4, 6, 2))
+        if shadow_corner1:
+            pygame.draw.rect(surface, self.palette[4], (4, 4, 6, 2))
+        if shadow_corner2:
+            pygame.draw.rect(surface, self.palette[4], (4, 4, 2, 4))
+        return surface
+
+class Button():
+    @staticmethod
+    def new(size: tuple,
+            text: str = "",
+            hover: bool = False,
+            unavailable: bool = False,
+            rounded_corners: Union[bool, list] = [True, True, True, True],
+            shadows: Union[bool, list] = [True, True, False, False]) -> pygame.Surface:
+        if type(rounded_corners) == bool:
+            rounded_corners = [rounded_corners]*4
+        elif len(rounded_corners) == 4:
+            rounded_corners = rounded_corners
+        else:
+            raise ValueError("rounded_corners must be of type bool or list[bool; 4]")
+
+        if type(shadows) == bool:
+            shadows = [shadows]*4
+        elif len(shadows) == 4:
+            shadows = shadows
+        else:
+            raise ValueError("shadows must be of type bool or list[bool; 4]")
+        
+        if size[0] == size[1]:
+            button = SquareButton(size, text, hover, unavailable, rounded_corners, shadows)
+        else:
+            button = RectButton(size, text, hover, unavailable, rounded_corners, shadows)
+        
+        return button.surface
+    
+    @staticmethod
+    def new_auto_pad(text: str = "",
+                     padding: int = 6,
+                     hover: bool = False,
+                     unavailable: bool = False,
+                     rounded_corners: Union[bool, list] = [True, True, True, True],
+                     shadows: Union[bool, list] = [True, True, False, False]) -> pygame.Surface:
+        _text = FONT.render(text, False, (239, 229, 206))
+
+        width = _text.get_width()
+        height = _text.get_height()
+        size = (width + padding + 12, height + padding + 10)
+
+        button = Button.new(size, text, hover, unavailable, rounded_corners, shadows)
+        return button
